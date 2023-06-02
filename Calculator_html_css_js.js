433235076ec9c15299c463439d2cff9a65d23546
rohitprocode <rohit990797@gmail.com>
2023-06-02 23:31:01 +0530

    Calculator Creation

--- Calculator_html_css_js.js
+++ Calculator_html_css_js.js
@@ -1,2 +1,26 @@
 let input = document.getElementById('inputBox');
 let buttons = document.querySelectorAll('button');
+
+let string = "";
+let arr = Array.from(buttons);
+    arr.forEach(button => {
+        button.addEventListener('click', (e) => {
+            if(e.target.innerHTML == '='){
+// sourcery skip: no-eval
+                string = eval(string);
+                input.value = string;
+            }
+            else if(e.target.innerHTML == 'AC'){
+                string = "";
+                input.value = string;
+            }
+            else if(e.target.innerHTML == 'DEL'){
+                
+                input.value = string;
+            }
+            else{
+                string += e.target.innerHTML;
+                input.value = string;
+            }
+    })
+} )
