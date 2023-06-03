2525dcdee96b940fac4d4f9a4dcadafc4bcf4905
rohitprocode <rohit990797@gmail.com>
2023-06-03 23:55:07 +0530

    Code

--- Calculator_html_css_js.js
+++ Calculator_html_css_js.js
@@ -1,6 +1,6 @@
 let input = document.getElementById('inputBox');
 let buttons = document.querySelectorAll('button');
-
+let a;
 let string = "";
 let arr = Array.from(buttons);
 arr.forEach(button => {
