f99a96e522cb15ae62eb819226c006fceb7990bb
rohitprocode <rohit990797@gmail.com>
2023-06-07 00:19:16 +0530

    Code explain

--- Calculator_html_css_js.js
+++ Calculator_html_css_js.js
@@ -2,8 +2,8 @@ let input = document.getElementById('inputBox');
 let buttons = document.querySelectorAll('button');
 let string = "";
 let arr = Array.from(buttons);
-arr.forEach(button => {
-    button.addEventListener('click', (e) => {
+arr.forEach(button => {                             //You can write any suitable word at the place of button
+    button.addEventListener('click', (e) => {       //You can write any suitable word at the place of button
         if(e.target.innerHTML == '='){
 
             string = eval(string);
@@ -23,3 +23,26 @@ arr.forEach(button => {
         }
 })
 } )
+
+// for(let i=0; i<arr.length; i++){
+//     let button = arr[i];
+//     button.addEventListener('click', (e) => {
+//         if(e.target.innerHTML == '='){
+
+//             string = eval(string);
+//             input.value = string;
+//         }
+//         else if(e.target.innerHTML == 'AC'){
+//             string = "";
+//             input.value = string;
+//         }
+//         else if(e.target.innerHTML == 'DEL'){
+//             string = string.substring( 0 , string.length -1);
+//             input.value = string;
+//         }
+//         else{
+//             string += e.target.innerHTML;
+//             input.value = string;
+//         }
+// })
+// }
